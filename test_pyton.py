abc3a328ec1b4d34c20418da36c05f65d34a9013
Sergey <bancone@mail.ru>
2019-09-25 18:44:24 +0300

    добавлен проект и тест

---
+++ test_pyton.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class UntitledTestCase(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.katalon.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_untitled_test_case(self):
+        driver = self.driver
+        driver.get("http://br0-admin.test7.iconic.local:81/login")
+        driver.find_element_by_name("login").clear()
+        driver.find_element_by_name("login").send_keys("root")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("root")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Password'])[1]/following::button[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='ti'])[1]/following::button[7]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Operators'])[1]/following::button[1]").click()
+        driver.find_element_by_name("email").click()
+        driver.find_element_by_name("email").clear()
+        driver.find_element_by_name("email").send_keys("111+1@1111.11")
+        driver.find_element_by_name("password").clear()
+        driver.find_element_by_name("password").send_keys("111111111")
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Role'])[1]/following::div[4]").click()
+        driver.find_element_by_xpath("//div[@id='react-select-5-option-1']").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='Broker Admin'])[2]/following::button[1]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='BROKER_ADMIN'])[11]/following::div[2]").click()
+        driver.find_element_by_xpath("(.//*[normalize-space(text()) and normalize-space(.)='ro'])[1]/following::button[1]").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+    
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+    
+    def tearDown(self):
+        self.driver.quit()
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
