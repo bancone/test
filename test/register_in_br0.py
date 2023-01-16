7e6af89544229851bc514c56a22cced9fd02143b
bancone <55791428+bancone@users.noreply.github.com>
2023-01-16 15:49:24 +0300

    Update register_in_br0.py

--- test/register_in_br0.py
+++ test/register_in_br0.py
@@ -12,5 +12,5 @@ def rpapp(request):
 
 
 def test_create_account(rpapp):
-    rpapp.create_account.login_bro(email=f's.pobedinskiy+{datetime.today().strftime("%y%m%d%H%M%S")}@iconic.vc', password="1234567890qwerty!")
-    #rpapp.create_qt.start_test(email="s.pobedinskiy+2002251@iconic.vc", password="1234567890qwerty!")
+    rpapp.create_account.login_bro(email=f'login+{datetime.today().strftime("%y%m%d%H%M%S")}@iconic.vc', password="password")
+    #rpapp.create_qt.start_test(email="login+2002251@iconic.vc", password="password")
